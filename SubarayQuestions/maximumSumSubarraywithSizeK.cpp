aebff225d4600114354bb95850948fc24f2101de
Gajendra singh dhaked <candidmastergajendra@gmail.com>
2024-08-21 01:21:50 +0530

    Update maximumSumSubarraywithSizeK.cpp

--- SubarayQuestions/maximumSumSubarraywithSizeK.cpp
+++ SubarayQuestions/maximumSumSubarraywithSizeK.cpp
@@ -1,1 +1,22 @@
-
+long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        // code here 
+        long ans = 0;
+        long sum = 0;
+        int index = 0;
+        for(;index < K; index++){
+            sum += Arr[index];
+        }
+        
+        if(index == K) ans = sum;
+        
+        for(int i = K; i < N; i++){
+            sum += Arr[i];
+            sum -= Arr[i-K];
+            
+            ans = max(ans, sum);
+        }
+        
+        return ans;
+        
+        
+    }
